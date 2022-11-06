b97a6953b90fcfa99e9d540216689cf000957ce1
kyudong_kim <kyudong_kim@tmax.co.kr>
2022-11-06 22:11:02 +0900

    Revert "Revert "add: 정답 파란색 표시, 다음 문제 넘어가기, 다 풀면 끝내기""
    This reverts commit a2d4041d28d878707a1e419a5a5bdb36a6c64169.

---
+++ src/components/QuestionBody.jsx
@@ -0,0 +1,91 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
+
+const Label = styled.label`
+  ${(props) =>
+    props.isAnswered &&
+    props.isCorrectAnswer &&
+    css`
+      color: blue;
+      font-weight: bold;
+    `}
+`;
+
+const QuestionBody = ({
+  data,
+  handleCorrectAnswer,
+  isAnswered,
+  setIsAnswered,
+}) => {
+  if (!data) return null;
+
+  const [answers, setAnswers] = useState([]);
+
+  const { category, correct_answer, difficulty, incorrect_answers, question } =
+    data;
+
+  useEffect(() => {
+    const newAnswers = [...incorrect_answers, correct_answer];
+    newAnswers.sort(() => Math.random() - 0.5);
+    setAnswers(newAnswers);
+  }, [data]);
+
+  const isCorrectAnswer = useCallback(
+    (selectedAnswer) => {
+      return selectedAnswer === correct_answer;
+    },
+    [correct_answer],
+  );
+
+  const handleChange = (e) => {
+    if (isCorrectAnswer(e.target.value)) {
+      alert('정답!');
+      handleCorrectAnswer();
+    } else {
+      alert('오답!');
+    }
+    setIsAnswered(true);
+  };
+
+  return (
+    <ul>
+      <li>카테고리: {category}</li>
+      <li>난이도: {difficulty}</li>
+      <li>문제: {question}</li>
+      <li>
+        보기
+        {answers.map((answer, index) => {
+          return (
+            <div key={`${answer.substr(0, 10)}${index}`}>
+              <input
+                type="radio"
+                name="answer"
+                id={answer}
+                value={answer}
+                onChange={handleChange}
+                disabled={isAnswered}
+              />
+              <Label
+                isAnswered={isAnswered}
+                isCorrectAnswer={isCorrectAnswer(answer)}
+                htmlFor={answer}
+              >
+                {answer}
+              </Label>
+            </div>
+          );
+        })}
+      </li>
+    </ul>
+  );
+};
+
+export default QuestionBody;
+
+QuestionBody.propTypes = {
+  data: PropTypes.object.isRequired,
+  handleCorrectAnswer: PropTypes.func.isRequired,
+  isAnswered: PropTypes.bool.isRequired,
+  setIsAnswered: PropTypes.func.isRequired,
+};
