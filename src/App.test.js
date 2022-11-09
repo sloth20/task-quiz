0735e02fa8f3e483fe9753dbd7b675b8847c2f72
kyudong_kim <kyudong_kim@tmax.co.kr>
2022-11-09 11:07:27 +0900

    fix: 테스트 수정

--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
-import App from './App';
 import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+
+import App from './App';
 
 describe('renders learn react link', () => {
   it('should render App', async () => {
     render(<App />);
 
     await waitFor(() => {
-      expect(
-        screen.getByRole('button', { name: '퀴즈 풀기' }),
-      ).toBeInTheDocument();
+      expect(screen.getByText('퀴즈 풀기')).toBeInTheDocument();
     });
 
-    const target = screen.getByRole('button', { name: '퀴즈 풀기' });
+    const target = screen.getByText('퀴즈 풀기');
     userEvent.click(target);
   });
 });
